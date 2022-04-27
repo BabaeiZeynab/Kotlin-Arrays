0d76400182fd8b5dee6d9188b24e46adbc877494
SafaBabaei <zeyneb.babaei@gmail.com>
2022-04-27 10:44:50 -0400

    arrays in Kotlin for begginers looping through arraysand sorting

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+fun main(args: Array<String>) {
+
+    // Define array in Kotlin
+
+    var arr : Array<Int> = arrayOf(22,44,11,66,33,55,88,99,100)
+
+
+    // get array size
+    var sizeOfarr = arr.size
+    println(sizeOfarr)
+
+
+    // Use for Loop to print array
+    for(i in arr){
+        println(i)
+    }
+
+
+    // use for Loop to print arry backward
+    for(i in arr.size-1 downTo 0){
+        println("index $i: ${arr[i]}")
+
+    }
+
+
+    // print arr with for each
+    arr.forEachIndexed { index, i ->
+        println("$index: $i")
+    }
+
+
+
+
+    // How to sort array in Kotlin
+
+    arr.sort()
+    arr.forEachIndexed { index, i ->
+        println("index $index: $i")
+    }
+}
